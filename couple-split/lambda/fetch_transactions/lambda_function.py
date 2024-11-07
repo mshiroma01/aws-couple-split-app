93d98f4c395e44579bc7bd7b1373348c43baa3a0
SaltyInter <mshiroma01@gmail.com>
2024-11-06 19:38:02 -0800

    updated to allow the ability to use the new csv_date_added field

--- couple-split/lambda/fetch_transactions/lambda_function.py
+++ couple-split/lambda/fetch_transactions/lambda_function.py
@@ -34,22 +34,30 @@ def lambda_handler(event, context):
         }
 
     try:
-        query_params = event['queryStringParameters']
+        query_params = event.get('queryStringParameters', {}) or {}
         userid = query_params.get('userid')
         status = query_params.get('status', 'pending')  # Default status is 'pending'
-        start_date = query_params.get('startDate')
-        end_date = query_params.get('endDate')
+        start_date = query_params.get('transactionStartDate')
+        end_date = query_params.get('transactionEndDate')
+        csv_start_date = query_params.get('csvStartDate')
+        csv_end_date = query_params.get('csvEndDate')
 
-        logger.info(f"Fetching transactions for user: {userid}, status: {status}, startDate: {start_date}, endDate: {end_date}")
+        logger.info(f"Fetching transactions for user: {userid}, status: {status}, transaction date range: {start_date} - {end_date}, CSV date range: {csv_start_date} - {csv_end_date}")
 
-        # Build the filter expression based on status and userid
-        filter_expression = Attr('userid').eq(userid) & Attr('status').eq(status)
+        # Build the filter expression based on userid and status
+        filter_expression = Attr('userid').eq(userid) & Attr('status').eq(status) & Attr('amount').lte(0)
 
-        # Add optional date filtering
+        # Add optional transaction_date filtering
         if start_date:
             filter_expression = filter_expression & Attr('transaction_date').gte(start_date)
         if end_date:
             filter_expression = filter_expression & Attr('transaction_date').lte(end_date)
+
+        # Add optional date_csv_added filtering
+        if csv_start_date:
+            filter_expression = filter_expression & Attr('date_csv_added').gte(csv_start_date)
+        if csv_end_date:
+            filter_expression = filter_expression & Attr('date_csv_added').lte(csv_end_date)
 
         # Perform a scan operation with the filter expression
         response = table.scan(
